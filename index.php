6040bcc0bfbcd455f397933639127ce9c0d092d0
GabrielMunguia <78899473+GabrielMunguia@users.noreply.github.com>
2022-04-04 18:56:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 try {
     define('DOCROOT', realpath(dirname(__FILE__)).'/');
 
-    include_once('controller\principal.php');
+    include_once('controller/principal.php');
     echo "123123";
 } catch (\Throwable $th) {
     echo $th->getMessage();
